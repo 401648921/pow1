85a37a3dcbf50837cb035200ef54e485565cc326
401648921 <58549791+401648921@users.noreply.github.com>
2020-01-07 22:15:13 +0800

    Add files via upload

---
+++ 学生管理系统/学生管理系统/src/ManagementSystem.java
@@ -0,0 +1,10 @@
+public interface ManagementSystem {
+    public abstract void inputStudent();
+    public abstract void inputClass();
+    public abstract void selectId();
+    public abstract void selectClass();
+    public abstract void alterStudent();
+    public abstract void alterClass();
+    public abstract void remove();
+    public abstract void selcetChoice(int flag);
+}
